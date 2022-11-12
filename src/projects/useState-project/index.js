8e7529701a3b28e8d2d0fee7d4f0c58b5e66a52b
Ankit Kumar <ankitkumar@Ankits-MacBook-Air.local>
2022-11-12 13:06:04 +0530

    usestateapp: alert added for num greater than 10

--- src/projects/useState-project/index.js
+++ src/projects/useState-project/index.js
@@ -1,21 +1,22 @@
 import { useState } from 'react';
 import './style.css';
-import Card from './components/Card';function IncreaseDecrease(){
-    const [num,setNum]=useState(10);
-    const increase = () =>{
-       num<10 && setNum(num+1);
+import Card from './components/Card'; function IncreaseDecrease() {
+    const [num, setNum] = useState(10);
+    const increase = () => {
+        num < 10 ? setNum(num + 1) : alert("hathi");
     }
-    const decrease = () =>{
-      num>0 &&  setNum(num-1);
+    const decrease = () => {
+        num > 0 && setNum(num - 1);
     }
-    return(
+    return (
         <div className="container">
-          
-            <div className='cardContainer' style ={{
-                backgroundColor:`rgb(255,255,${255-25.5*num})` }}>
-<Card content ={'+'} colour = {'red'} counterHandler={increase} />
-<div>{num}</div>
-<Card content= {'-'} colour ={ "green" } counterHandler={decrease}/>
+
+            <div className='cardContainer' style={{
+                backgroundColor: `rgb(255,255,${255 - 25.5 * num})`
+            }}>
+                <Card content={'+'} colour={'red'} counterHandler={increase} />
+                <div>{num}</div>
+                <Card content={'-'} colour={"green"} counterHandler={decrease} />
             </div>
         </div>
     )
